76d0afcbe1ee7dbbc6562bcfcfbccc740c5840ee
kaustubhdeokar <kaustubhd9@gmail.com>
2023-11-29 11:34:39 +0530

    using state to change bg colors onclick

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,12 +5,22 @@ import './App.css'
 import Cards from './components/Cards'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
+
+  function changeColor(color) {
+    setColor(color);
+  }
 
   return (
     <>
-     <h1 className='text-3xl bg-green-500 p-4 rounded-md'>Vite with Tailwind</h1>
-     <Cards username="kd" />
+      <div className="h-screen flex items-center justify-center" style={{ backgroundColor: color }}>
+        <button onClick={() => changeColor('green')}
+          type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Green</button>
+        <button onClick={() => changeColor('red')}
+          type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
+        <button onClick={() => changeColor('yellow')}
+          type="button" className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900">Yellow</button>
+      </div>
     </>
   )
 }
